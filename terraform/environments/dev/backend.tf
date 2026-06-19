5e08747e3445a92d8f41022f353cacdad56fd67c
stephanie <232054776+stephcloud@users.noreply.github.com>
2026-06-19 04:12:59 +0100

    Update S3 backend config for dev and prod environments

--- terraform/environments/dev/backend.tf
+++ terraform/environments/dev/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket         = "petclinic-terraform-state"
+    bucket         = "petclinic-terraform-state-720035686687"
     key            = "petclinic/dev/terraform.tfstate"
-    region         = "eu-central-1"
+    region         = "us-east-1"
     dynamodb_table = "petclinic-terraform-locks"
     encrypt        = true
   }
